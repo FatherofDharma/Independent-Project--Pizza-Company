7a7876547b609aed545356f810127d2f112dc43c
FatherofDharma <fatherofdharma@gmail.com>
2019-06-21 09:13:04 -0700

    add orderlist id's and rename some object properties, to make their purpose easier to understand.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
-function Orders() {
-  this.pizzas = []
+//Business logic section.
+
+function OrderList() {
+  this.orders = [],
+  this.ordersId = 0;
 }
 
-function Pizza (){
+Orders.prototype.addOrder = function() {
+
+}
+
+function Order (name, pizza) {
+  this.name = name,
+  this.pizzas = pizzas,
+  this.currentorderId = currentOrderList.ordersId,
+  currentOrderList.ordersId += 1;
+}
+
+function Pizza (name, size, toppings, ) {
+  this.name = name,
   this.size = size,
   this.toppings = toppings,
 }
+
+var currentOrderList = new OrderList();
+
+//User interface logic section.
